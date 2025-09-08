a3c02c6334cd2c6c4dee8188c3fa474074249382
Apoorv <iron.spidey1230@gmail.com>
2025-09-08 14:39:06 +0530

    Change 5

---
+++ generators/animation_generator.py
@@ -0,0 +1,4 @@
+# inside generators/music_generator.py
+class AnimationGenerator:
+    def __init__(self):
+        pass
---
+++ generators/music_generator.py
@@ -0,0 +1,4 @@
+# inside generators/music_generator.py
+class MusicGenerator:
+    def __init__(self):
+        pass
